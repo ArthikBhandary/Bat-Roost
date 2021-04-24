1b660728d393715a2822730e2e92304b9d71de9b
ashish b <ashishb2013@gmail.com>
2021-04-24 23:37:00 +0530

    second

--- bat_roost/bat_roost/settings.py
+++ bat_roost/bat_roost/settings.py
@@ -63,7 +63,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'bat_roost/templates'),
+            os.path.join(BASE_DIR,'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
