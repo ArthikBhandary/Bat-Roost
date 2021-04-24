a4a3e98ab6803006e25de8958dc656bf081ab7f2
ArthikBhandary <arthikrb@gmail.com>
2021-04-24 23:03:57 +0530

    Login and user details API, and password reset Link

--- bat_roost/bat_roost/urls.py
+++ bat_roost/bat_roost/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView, PasswordResetDoneView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('submission/', include('submission.urls')),
+    path('forgot_password/', PasswordResetView.as_view(), name="password_reset"),
+    path('password_reset_done/', PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset_password/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('', include('core.urls')),
 ]
---
+++ bat_roost/core/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from rest_auth.views import LoginView as LoginApiView
+from core.api.views import UserDataAPIView
+
+
+app_name = "core"
+
+
+urlpatterns = [
+    path("login/", LoginApiView.as_view(), name='api-login'),
+    path("user/", UserDataAPIView.as_view(), name="api-details"),
+]
