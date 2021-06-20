183178951d82ef44c930260d84fec0a614553c4b
ArthikBhandary <arthikrb@gmail.com>
2021-06-20 22:47:34 +0530

    Download API added

--- bat_roost/submission/api/urls.py
+++ bat_roost/submission/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from submission.api.views import SubmissionListApiView, SubmissionCreateAPIView, SubmissionDetailApiView
+from submission.api import views
 
 app_name = "submission"
 
 
 urlpatterns = [
-    path("list/", SubmissionListApiView.as_view(), name="list"),
-    path("create/", SubmissionCreateAPIView.as_view(), name="create"),
-    path("details/", SubmissionDetailApiView.as_view(), name="details"),
+    path("list/", views.SubmissionListApiView.as_view(), name="list"),
+    path("create/", views.SubmissionCreateAPIView.as_view(), name="create"),
+    path("details/", views.SubmissionDetailApiView.as_view(), name="details"),
+    path("download/", views.SubmissionListDownloadApiView.as_view(), name="details"),
 ]
--- bat_roost/submission/api/views.py
+++ bat_roost/submission/api/views.py
@@ -5,6 +5,8 @@ from submission.api.serializers import SubmissionSerializer, SubmissionDetailSer
 from submission.models import Submission
 
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+import csv
 
 class SubmissionCreateAPIView(CreateAPIView):
     permission_classes = [IsAuthenticated]
@@ -26,3 +28,22 @@ class SubmissionDetailApiView(RetrieveAPIView):
     serializer_class = SubmissionDetailSerializer
     def get_object(self):
         return get_object_or_404(Submission,pk=self.request.GET.get("pk"))
+
+
+
+class SubmissionListDownloadApiView(ListAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        items = Submission.objects.filter(status=Submission.ACCEPTED, user=request.user)
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="submission.csv"'
+        writer = csv.writer(response, delimiter=',')
+        writer.writerow(['user', 'description', 'approx_bats',
+                         'submission_time', 'photo_taken_time'])
+
+        for obj in items:
+            writer.writerow([obj.user, obj.description, obj.approx_bats,
+                             obj.submission_time, obj.photo_taken_time])
+
+        return response
