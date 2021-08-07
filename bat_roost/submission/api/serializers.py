953ef5f71eaa939a9d117333e92b801390cf8b4e
ArthikBhandary <arthikrb@gmail.com>
2021-08-07 23:47:12 +0530

    Species details in API

--- bat_roost/submission/api/serializers.py
+++ bat_roost/submission/api/serializers.py
@@ -22,22 +22,22 @@ class SpeciesSerializer(serializers.ModelSerializer):
 
 class SubmissionSerializer(serializers.ModelSerializer):
     images = ImageSerializer(read_only=True, many=True)
-
+    species = SpeciesSerializer(read_only=True, many=True)
     class Meta:
         model = Submission
-        fields = ("pk", "description", "approx_bats",
-                  "photo_taken_time", "images",  "latitude", "longitude", "review")
-        read_only_fields = ["pk", "images",  "latitude", "longitude", "review"]
+        fields = ("pk", "status", "description", "approx_bats", "submission_time",
+                  "photo_taken_time",  "images",  "latitude", "longitude", "review", "species")
+        read_only_fields = ["pk", "images", "review", "species"]
 
 
 class SubmissionListSerializer(serializers.ModelSerializer):
     images = ImageSerializer(read_only=True, many=True)
-
+    species = SpeciesSerializer(read_only=True, many=True)
     class Meta:
         model = Submission
         fields = ("pk", "status", "description", "approx_bats", "submission_time",
-                  "photo_taken_time", "images", "latitude", "longitude", "review")
-        read_only_fields = ["pk", "images", "review"]
+                  "photo_taken_time",  "images",  "latitude", "longitude", "review", "species")
+        read_only_fields = ["pk", "images", "review", "species"]
 
 
 class SubmissionDetailSerializer(serializers.ModelSerializer):
