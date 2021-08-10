3ddb74ba1d9db96f83323122eaaee30b7b296c66
ArthikBhandary <arthikrb@gmail.com>
2021-08-10 23:01:32 +0530

    TimeZone Correction

--- bat_roost/submission/models.py
+++ bat_roost/submission/models.py
@@ -1,10 +1,8 @@
 from django.contrib.gis.db import models as gmodels
 from django.db import models
-
 from django.urls import reverse
+from django.utils import timezone
 from submission.misc_functions import image_name
-from django.contrib.gis.geos import Point
-from django.db.models.functions import Cast
 
 
 class Species(models.Model):
@@ -42,7 +40,7 @@ class Submission(gmodels.Model):
     user = gmodels.ForeignKey("core.User", on_delete=models.CASCADE)
     description = gmodels.TextField(blank=True)
     approx_bats = gmodels.PositiveIntegerField()
-    submission_time = gmodels.DateTimeField(auto_now_add=True)
+    submission_time = gmodels.DateTimeField(default=timezone.now)
     rejected_time = gmodels.DateTimeField(blank=True, null=True)
     review = gmodels.TextField(blank=True)
     photo_taken_time = gmodels.DateTimeField()
@@ -53,11 +51,14 @@ class Submission(gmodels.Model):
 
     @property
     def latitude(self):
-        return self.location.x
+        return self.location.y
 
     @property
     def longitude(self):
-        return self.location.y
+        return self.location.x
+
+    def __str__(self):
+        return str(self.pk) + self.user.username
 
     def get_all_species_string(self):
         return ", ".join(str(specie.name) for specie in self.species.all())
@@ -75,7 +76,7 @@ class Submission(gmodels.Model):
 
     def delete(self, *args, **kwargs):
         self.images.all().delete()
-        super(Recipe, self).delete(*args, **kwargs)
+        super(Submission, self).delete(*args, **kwargs)
 
 
 class SubmissionImage(models.Model):
@@ -86,7 +87,7 @@ class SubmissionImage(models.Model):
     def delete(self, *args, **kwargs):
         if self.image:  # If image isn't empty, delete image from storage
             storage, path = self.image.storage, self.image.path
-            super(Recipe, self).delete(*args, **kwargs)
+            super(SubmissionImage, self).delete(*args, **kwargs)
             storage.delete(path)
         else:
-            super(Recipe, self).delete(*args, **kwargs)
+            super(SubmissionImage, self).delete(*args, **kwargs)
