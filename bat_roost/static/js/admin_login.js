f763c91279df13287b5a1f86ee2e37509ff79014
ArthikBhandary <arthikrb@gmail.com>
2021-08-14 22:02:21 +0530

    admin login css and js style update

--- bat_roost/static/js/admin_login.js
+++ bat_roost/static/js/admin_login.js
@@ -1,11 +1,6 @@
 'use strict';
 
-console.log("Here");
 $(function() {
-
-	// author badge :)
-	var author = '<div style="position: fixed;bottom: 0;right: 20px;background-color: #fff;box-shadow: 0 4px 8px rgba(0,0,0,.05);border-radius: 3px 3px 0 0;font-size: 12px;padding: 5px 10px;">By <a href="https://twitter.com/mhdnauvalazhar">@mhdnauvalazhar</a> &nbsp;&bull;&nbsp; <a href="https://www.buymeacoffee.com/mhdnauvalazhar">Buy me a Coffee</a></div>';
-	$("body").append(author);
 
 	$("input[type='password']").each(function(i) {
 		var $this = $(this),
@@ -63,10 +58,6 @@ $(function() {
 
 	$(".my-login-validation").submit(function() {
 		var form = $(this);
-        // if (form[0].checkValidity() === false) {
-        //   event.preventDefault();
-        //   event.stopPropagation();
-        // }
 		form.addClass('was-validated');
 	});
 });
