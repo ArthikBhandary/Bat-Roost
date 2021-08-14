e18ce709239c9e567994038c49518386ad70fb5e
ArthikBhandary <arthikrb@gmail.com>
2021-08-14 21:57:49 +0530

    Email confirmed url update

--- bat_roost/core/urls.py
+++ bat_roost/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from core.views import AdminLoginView, ThanksView, test_view
+from core.views import AdminLoginView, ThanksView, EmailConfirmedView
 
 app_name = "core"
 
@@ -14,5 +14,5 @@ urlpatterns = [
     path('thanks/', ThanksView.as_view(), name="thanks"),
     path(endpoint, include('core.api.urls')),
     path(endpoint + "submission/", include('submission.api.urls')),
-    path("test",test_view.as_view(), name="test")
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("email_confirmed/",EmailConfirmedView.as_view(), name="email_confirmed")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
